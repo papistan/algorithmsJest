a86bb55a4bdfd2a64d9a82bf93e4094dfc6d8da1
papistan <jpapisan@gmail.com>
2018-04-19 20:27:19 -0700

    Completes chunk excercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let chunkedArray = [];
+    while (array.length > size) {
+        chunkedArray.push(array.splice(0, size))
+    }
+    if (array.length >= 1) {
+        chunkedArray.push(array)
+    }
+    return chunkedArray
+}
 
 module.exports = chunk;
